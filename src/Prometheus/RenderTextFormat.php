15c1d5c59b8feb2901c20b95bc5713ff35d20cf5
Michael Leiss <michael.leiss@cartware.de>
2023-11-28 15:22:54 +0100

    Add PHP8 compatibility

--- src/Prometheus/RenderTextFormat.php
+++ src/Prometheus/RenderTextFormat.php
@@ -56,7 +56,7 @@ class RenderTextFormat
      */
     private function escapeLabelValue($v): string
     {
-        $v = str_replace("\\", "\\\\", $v);
+        $v = str_replace("\\", "\\\\", (string) $v);
         $v = str_replace("\n", "\\n", $v);
         $v = str_replace("\"", "\\\"", $v);
         return $v;
